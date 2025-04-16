c183d24e92c4a6a313a82e5d313feec972b172a6
Alex Bruch <alex.bruch@tutanota.com>
2025-04-16 14:42:09 +0200

    init

---
+++ voebb/src/main/java/com/example/voebb/VoebbApplication.java
@@ -0,0 +1,13 @@
+package com.example.voebb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoebbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoebbApplication.class, args);
+	}
+
+}
