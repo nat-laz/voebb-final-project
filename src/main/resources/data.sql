6ec07ce53e6d3fc67cfdc7e46949712f4d3c6ec1
bruch-alex <alex.bruch@tutanota.com>
2025-04-23 17:14:45 +0200

    add product-types into data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
+-- Product types
+INSERT INTO product_types (id, name, is_digital) VALUES (1,'book', false) ON CONFLICT (id) DO NOTHING;
+INSERT INTO product_types (id, name, is_digital) VALUES (2,'ebook', true) ON CONFLICT (id) DO NOTHING;
+-- Products
 INSERT INTO products (id, title, product_type_id) VALUES (1,'Harry Potter and the Philosopher''s Stone', 1) ON CONFLICT (id) DO NOTHING;
 INSERT INTO products (id, title, product_type_id) VALUES (2,'Harry Potter and the Chamber of Secrets', 2) ON CONFLICT (id) DO NOTHING;
 INSERT INTO products (id, title, product_type_id) VALUES (3,'Harry Potter and the Prisoner of Azkaban', 1) ON CONFLICT (id) DO NOTHING;
