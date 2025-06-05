901e199c25076a84e52e23df32af71b9cee4b515
Alex Bruch <alex.bruch@tutanota.com>
2025-06-05 18:42:37 +0200

    fix bug with empty creators

--- src/main/java/com/example/voebb/model/dto/creator/UpdateCreatorWithRoleDTO.java
+++ src/main/java/com/example/voebb/model/dto/creator/UpdateCreatorWithRoleDTO.java
@@ -1,14 +1,12 @@
 package com.example.voebb.model.dto.creator;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class UpdateCreatorWithRoleDTO {
     private Long creatorId;
     private String firstName;
